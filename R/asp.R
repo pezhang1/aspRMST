268bfa247511b967b1eea2aaf1ac98d455b2e763
Peter Zhang <pezhang@mcw.edu>
2024-01-20 13:36:12 -0600

    first

---
+++ R/asp.R
@@ -0,0 +1,128 @@
+#' Title
+#'
+#' @param t0 - time point of analysis
+#' @param Time - Observed Times
+#' @param Status - Censoring Status
+#' @param Z - non-treatment groupcovariates
+#' @param TRT - treatment group indicator
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' library("survival)
+#' t0 = 1
+#' TRT = c(rep(0,5),rep(1,5))
+#' Z = cbind(rnorm(10))
+#' FT = c(0.30,  0.58,  0.41,  0.0333,  0.58,  0.10,  0.83,  2.45,  8.7, 17.1)
+#' CT = c(0.20, 0.8, 0.73, 0.24, 0.5, 0.25, 1.3, 3.6, 10, 20.5)
+#' X = pmin(FT,CT)
+#' Status = as.numeric(FT <= CT)     & (X <= t0)
+#' Time = pmin(X,t0)
+#' asp(t0,Time,Status,Z,TRT)$SPD
+#' asp(t0,Time,Status,Z,TRT)$SE2
+asp = function(t0,Time,Status,Z,TRT) {
+n= length(TRT)
+n1 = sum(TRT)
+n0 = n-n1
+
+#run stratified Cox model
+Cox = coxph(Surv(Time,Status) ~ Z + strata(TRT))
+beta = cbind(as.numeric(coef(Cox)))
+SigmaInv = as.matrix(vcov(Cox))
+
+Time0 = Time[TRT==0]
+Time1 = Time[TRT==1]
+Status0 = Status[TRT==0]
+Status1 = Status[TRT==1]
+Z0 = as.matrix(subset(Z, subset= (TRT==0), drop = TRUE))
+Z1 = as.matrix(subset(Z, subset= (TRT==1), drop = TRUE))
+
+ZBETA  = as.numeric(Z%*%beta)
+ZBETA0 = as.numeric(Z0%*%beta)
+ZBETA1 = as.numeric(Z1%*%beta)
+
+
+X0 = unique(sort(Time0*Status0*(Time0<=t0)))
+X1 = unique(sort(Time1*Status1*(Time1<=t0)))
+
+
+temp1 = temp2 = NULL
+temp3 = temp4 = matrix(0,length(X0),dim(Z0)[2])
+for(k in 1:length(X0)) {
+  NR0 = as.numeric(Time0>=X0[k]) #Number at risk for group0
+  temp1[k] = sum(Status0[Time0==X0[k]])/sum(NR0*exp(ZBETA0))
+   temp2[k]  = n0*sum(Status0[Time0==X0[k]])/(sum(NR0*exp(ZBETA0))^2)
+  for(j in 1:dim(Z0)[2])
+  {
+    temp4[k,j] = sum(NR0*exp(ZBETA0)*Z0[,j])
+  }
+  temp3[k,] = sum(Status0[Time0==X0[k]])*temp4[k,]/(sum(NR0*exp(ZBETA0))^2)
+}
+Lambda00 = sum(temp1)
+gamma0 = sum(temp2)
+Q0 = cbind(apply(temp3,2,sum))
+
+
+temp5 = temp6 = NULL
+temp7 = temp8 = matrix(0,length(X1),dim(Z1)[2])
+for(k in 1:length(X1))
+{
+  NR1 = as.numeric(Time1>=X1[k])
+  temp5[k] = sum(Status1[Time1==X1[k]])/sum(NR1*exp(ZBETA1))
+  temp6[k]  = n1*sum(Status1[Time1==X1[k]])/(sum(NR1*exp(ZBETA1))^2)
+  for(j in 1:dim(Z1)[2])
+  {
+    temp8[k,j] = sum(NR1*exp(ZBETA1)*Z1[,j])
+  }
+  temp7[k,] = sum(Status1[Time1==X1[k]])*temp8[k,]/(sum(NR1*exp(ZBETA1))^2)
+}
+Lambda01 = sum(temp5)
+gamma1 = sum(temp6)
+Q1 = cbind(apply(temp7,2,sum))
+
+# Calculate marginal survival function estimate at t0
+# Calculate ci1(t) at t0
+# Calculate ci2(t) at t0
+temp1 = temp5 = temp2 = temp6 = NULL
+temp3 = temp7 = matrix(0,dim(Z)[1],dim(Z)[2])
+for(i in 1:dim(Z)[1])
+{
+  ZBETAi = as.numeric(Z[i,]%*%beta)
+  temp1[i] = exp(-exp(ZBETAi)*Lambda00)
+  temp5[i] = exp(-exp(ZBETAi)*Lambda01)
+  temp2[i]  = exp(-exp(ZBETAi)*Lambda00)*exp(ZBETAi)
+  temp6[i]  = exp(-exp(ZBETAi)*Lambda01)*exp(ZBETAi)
+  for(j in 1:dim(Z)[2])
+  {
+    temp3[i,j] = exp(-exp(ZBETAi)*Lambda00)*exp(ZBETAi)*Z[i,j]
+    temp7[i,j] = exp(-exp(ZBETAi)*Lambda01)*exp(ZBETAi)*Z[i,j]
+  }
+}
+CS0 = temp1
+CS1 = temp5
+S0  = mean(CS0)
+S1  = mean(CS1)
+SPD = mean(CS1-CS0)
+SPV = var(CS1-CS0)
+c01 = mean(temp2)
+c11 = mean(temp6)
+c02 = cbind(apply(temp3,2,mean))
+c12 = cbind(apply(temp7,2,mean))
+
+D0 = c01*Q0 - Lambda00*c02
+D1 = c11*Q1 - Lambda01*c12
+DD = D1-D0
+
+
+# Calculate standard error of SE difference
+V1 = c01^2*gamma0/n0 + c11^2*gamma1/n1 + t(DD)%*%SigmaInv%*%DD
+V2 = V1 + SPV/n
+SE1 = sqrt(V1)
+SE2 = sqrt(V2)
+
+return(list(Lambda00=Lambda00,Lambda01=Lambda01,S0=S0,S1=S1,SPD=SPD,SPV=SPV,V1=V1,V2=V2,SE1=SE1,SE2=SE2,CS0=CS0,CS1=CS1,
+            gamma0=gamma0,gamma1=gamma1,Q0=Q0,Q1=Q1,c01=c01,c11=c11,c02=c02,c12=c12,D0=D0,D1=D1,DD=DD,SigmaInv=SigmaInv,
+            X0=X0,X1=X1,n=n,n0=n0,n1=n1))
+}
---
+++ R/rmst.R
@@ -0,0 +1,136 @@
+#' Title
+#'
+#' @param t0 - time point of analysis
+#' @param Time - Observed Times
+#' @param Status - Censoring Status
+#' @param Z - non-treatment groupcovariates
+#' @param TRT - treatment group indicator
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' library("survival)
+#' t0 = 1
+#' TRT = c(rep(0,5),rep(1,5))
+#' Z = cbind(rnorm(10))
+#' FT = c(0.30,  0.58,  0.41,  0.0333,  0.58,  0.10,  0.83,  2.45,  8.7, 17.1)
+#' CT = c(0.20, 0.8, 0.73, 0.24, 0.5, 0.25, 1.3, 3.6, 10, 20.5)
+#' X = pmin(FT,CT)
+#' Status = as.numeric(FT <= CT)     & (X <= t0)
+#' Time = pmin(X,t0)
+#' rmst(t0,Time,Status,Z,TRT)$Delta
+#' rmst(t0,Time,Status,Z,TRT)$DSE2
+#'
+rmst = function(t0,Time,Status,Z,TRT){
+  # Calculate RMST difference and its standard error
+
+
+  SPSE = asp(t0,Time,Status,Z,TRT)
+  n0 = SPSE$n0
+  n1 = SPSE$n1
+  n  = SPSE$n
+  SigmaInv = SPSE$SigmaInv
+  x0 = SPSE$X0
+  x1 = SPSE$X1
+  dx0 = diff(c(x0,t0),lag=1)
+  dx1 = diff(c(x1,t0),lag=1)
+
+
+
+  # Calculate RMST difference
+  # Calculate B1i(u,t) and B2i(u,t) at t0.
+  # Calculate Psi(iu,t) at t0.
+  # Calculate B3(u,t) at t0.
+  # Calculate mui(u|Z) at t0.
+  sp0 = C01 = Gamma0 = Lambda0 = NULL
+  C02 = QQ0 = matrix(0,length(x0),dim(Z)[2])
+  cs0 = matrix(0,dim(Z)[1],length(x0))
+  for(k in 1:length(x0))
+  {
+    spse0      = asp(x0[k],Time,Status,Z,TRT)
+    sp0[k]     = spse0$S0
+    C01[k]     = spse0$c01
+    Gamma0[k]  = spse0$gamma0
+    Lambda0[k] = spse0$Lambda00
+    C02[k,]    = spse0$c02
+    QQ0[k,]    = spse0$Q0
+    cs0[,k]    = spse0$CS0
+  }
+  Temp0 = matrix(0,length(x0),length(x0))
+  diag(Temp0) = Gamma0
+  for(j in 1:(length(x0)-1))
+  {
+    for(k in (j+1):length(x0))
+    {
+      Temp0[j,k] = Temp0[k,j] = Gamma0[j]
+    }
+  }
+  mu0 = sum(sp0*dx0)
+  B10 = (n/n0)* rbind(C01*dx0) %*% Temp0 %*% cbind(C01*dx0)
+  Psi0 = t(QQ0)%*%cbind(C01*dx0) - t(C02)%*%cbind(Lambda0*dx0)
+  dgamma0 = c(0,diff(Gamma0,lag=1))
+  B30 = t(Psi0) %*% SigmaInv %*% Psi0
+  V10 = as.numeric( (B10+B30)/n )
+  SE10 = sqrt(V10)
+  cmu0 = as.numeric(cs0 %*% cbind(dx0))
+
+  sp1 = C11 = Gamma1 = Lambda1 = NULL
+  C12 = QQ1 = matrix(0,length(x1),dim(Z)[2])
+  cs1 = matrix(0,dim(Z)[1],length(x1))
+  for(k in 1:length(x1))
+  {
+    spse1      = asp(x1[k],Time,Status,Z,TRT)
+    sp1[k]     = spse1$S1
+    C11[k]     = spse1$c11
+    Gamma1[k]  = spse1$gamma1
+    Lambda1[k] = spse1$Lambda01
+    C12[k,]    = spse1$c12
+    QQ1[k,]    = spse1$Q1
+    cs1[,k]    = spse1$CS1
+  }
+  Temp1 = matrix(0,length(x1),length(x1))
+  diag(Temp1) = Gamma1
+  for(j in 1:(length(x1)-1))
+  {
+    for(k in (j+1):length(x1))
+    {
+      Temp1[j,k] = Temp1[k,j] = Gamma1[j]
+    }
+  }
+  mu1 = sum(sp1*dx1)
+  B11  = (n/n1)* rbind(C11*dx1) %*% Temp1 %*% cbind(C11*dx1)
+  Psi1 = t(QQ1)%*%cbind(C11*dx1) - t(C12)%*%cbind(Lambda1*dx1)
+  dgamma1 = c(0,diff(Gamma1,lag=1))
+  B31 = t(Psi1) %*% SigmaInv %*% Psi1
+  V11 = as.numeric( (B11+B31)/n )
+  SE11 = sqrt(V11)
+  cmu1 = as.numeric(cs1 %*% cbind(dx1))
+
+  Delta = mu1 - mu0
+  B3 = t(Psi1-Psi0) %*% SigmaInv %*% (Psi1-Psi0)
+  DV1 = as.numeric( (B10+B11+B3)/n )
+  DSE1 = sqrt(DV1)
+
+
+
+  # Calculate mean and variance of mu1(u|Z)-mu0(t|Z) at t0.
+  v10 = V10 + var(cmu0)/n
+  se10 = sqrt(v10)
+
+  v11 = V11 + var(cmu1)/n
+  se11 = sqrt(v11)
+
+  DeltaV = var(cmu1-cmu0)
+  DV2 = DV1 + DeltaV/n
+  DSE2 = sqrt(DV2)
+
+  list(mu0=mu0,mu1=mu1,Delta=Delta,DeltaV=DeltaV,DV1=DV1,DV2=DV2,DSE1=DSE1,DSE2=DSE2 )
+}
+
+
+
+
+
+
+
