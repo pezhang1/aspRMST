4183bf018e6992a7506c85ac9549e2d3c9d3ffb2
pezhang1 <pezhang@mcw.edu>
2024-01-21 15:27:27 -0600

    typo

--- R/rmst.R
+++ R/rmst.R
@@ -27,7 +27,7 @@
 #' @param t0 - time point of analysis
 #' @param Time - Observed Times
 #' @param Status - Censoring Status (0 = Control, 1 = Treatment)
-#' @param Z - non-treatment groupcovariates
+#' @param Z - non-treatment group covariates
 #' @param TRT - treatment group indicator (0 = Control, 1 = Treatment)
 #'
 #' @return
